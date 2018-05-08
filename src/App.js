36f74da3ab8b10ff1a97b0468affebd7ff14f5d4
Tyler Glisson <t2@I-Love-Kari.local>
2018-05-08 08:28:08 -0400

    First phase of converting the React App starter package into a React version of my Wiki Search.  Changes to App.js, all CSS files/references, and generally tearing everything down to barebones

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './stylesheets/style.css';
+//const Footero = ({namedContent}) => {
+
+const Appo= (props) => {
+  return (
+    <div className="App">
+
+    </div>
+  )
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {/*<Headero/>*/}
+        <Appo/>
+      </div>
+    );
+  }
+}
+
+export default App;
